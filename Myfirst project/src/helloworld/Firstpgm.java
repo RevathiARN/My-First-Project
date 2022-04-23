d1644f1935a5dbc203a0d1954b9e69f1265052d0
Lenovo <Lenovo@DESKTOP-N3OK6UO>
2022-04-23 10:23:11 +0530

    My First Commit

---
+++ Myfirst project/src/helloworld/Firstpgm.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class Firstpgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+		
+
+	}
+
+}
